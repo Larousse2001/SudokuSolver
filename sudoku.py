7aac16ebe03e7ebd57a3aaccacc1a4e622366804
Arous Achraf <87370368+Larousse2001@users.noreply.github.com>
2022-12-06 23:02:20 +0100

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -15,19 +15,19 @@ side  = base*base # initialization of the number of all the grid 9*9
 #Individuals : {"1":1  , "2":2 , "3":3 , "4":4 , "5":5 , "6":6 , "7":7 ,"8":8 ,"9":9 }
 
 # pattern for a baseline valid solution
-def pattern(r,c): return (base*(r%base)+r//base+c)%side # final population: a grid with 81 individuals such as for each row, each column and each grid of 3*3 has no duplicates.
+def pattern(r,c): return (base*(r%base)+r//base+c)%side # cross over: Iterating through the values of the domain of the variable, then we compare the existants values with the no-existants values by counting the number of legal variables on the other unassigned variables then updating the domain of the variable by sorting them by those that allow the most legal moves.
 
 #the first step is selection of individuals randomly
 # randomize rows, columns and numbers (of valid base pattern)
 from random import sample
-def shuffle(s): return sample(s,len(s)) # cross over: Iterating through the values of the domain of the variable, then we compare the existants values with the no-existants values by counting the number of legal variables on the other unassigned variables then updating the domain of the variable by sorting them by those that allow the most legal moves.
+def shuffle(s): return sample(s,len(s)) 
 rBase = range(base)  # initial population
 rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ] # fill the rows
 cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ] # fill the columns
 nums  = shuffle(range(1,base*base+1)) # mutation: it is able to replace a large portion of the population (worst performing chromosomes) with completely randomized boards, but this seems to have a minimal effect.
 
 # produce board using randomized baseline pattern
-board = [ [nums[pattern(r,c)] for c in cols] for r in rows]
+board = [ [nums[pattern(r,c)] for c in cols] for r in rows] # final population: a grid with 81 individuals such as for each row, each column and each grid of 3*3 has no duplicates.
 
 for c in cols: # fitness function
     for r in rows:
